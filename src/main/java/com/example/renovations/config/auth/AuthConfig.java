abd9328cf7150f0de0cacd0202aef2e04f8be282
Thibaut Virolle <thibaut.virolle@protonmail.com>
2025-01-17 08:39:51 +0100

    feat: enable CORS with config for localhost

--- src/main/java/com/example/renovations/config/auth/AuthConfig.java
+++ src/main/java/com/example/renovations/config/auth/AuthConfig.java
@@ -1,10 +1,13 @@
 package com.example.renovations.config.auth;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -13,6 +16,9 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
@@ -23,16 +29,16 @@ public class AuthConfig {
   @Bean
   SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
     return httpSecurity
+        .cors(Customizer.withDefaults()) // Needed for CORS with Spring Security
         .csrf(csrf -> csrf.disable())
         .formLogin(formLogin -> formLogin.disable())
         .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
         .authorizeHttpRequests(authorize -> authorize
-          .requestMatchers(HttpMethod.POST, "/api/v1/auth/*").permitAll()
-          .requestMatchers("/error").permitAll()
-          .requestMatchers("/h2-console*").permitAll()
-          .requestMatchers("/h2-console/*").permitAll()
-          .anyRequest().authenticated()
-        )
+            .requestMatchers(HttpMethod.POST, "/api/v1/auth/*").permitAll()
+            .requestMatchers("/error").permitAll()
+            .requestMatchers("/h2-console*").permitAll()
+            .requestMatchers("/h2-console/*").permitAll()
+            .anyRequest().authenticated())
         .headers(headers -> headers.frameOptions(frameOptionsConfig -> {
           // For h2-console
           frameOptionsConfig.disable();
@@ -52,5 +58,20 @@ public class AuthConfig {
   @Bean
   PasswordEncoder passwordEncoder() {
     return new BCryptPasswordEncoder();
+  }
+
+  /**
+   * Sets the configuration for CORS with
+   * `httpSecurity.cors(Customizer.withDefaults())`
+   */
+  @Bean
+  CorsConfigurationSource corsConfigurationSource() {
+    CorsConfiguration configuration = new CorsConfiguration();
+    configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
+    configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PATCH", "DELETE"));
+    configuration.setAllowedHeaders(Arrays.asList("authorization"));
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", configuration);
+    return source;
   }
 }
--- src/main/java/com/example/renovations/projects/ProjectsController.java
+++ src/main/java/com/example/renovations/projects/ProjectsController.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PostAuthorize;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,7 +19,6 @@ import jakarta.servlet.http.HttpServletRequest;
 
 @RestController
 @RequestMapping("/api/v1")
-@CrossOrigin(origins = "http://localhost:4200")
 public class ProjectsController {
     @Autowired
     private ProjectsService projectsService;
